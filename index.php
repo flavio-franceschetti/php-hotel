8d584d073d6c3e138a85ab07cf886fee0475aa42
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-07-24 15:54:12 +0200

    feat: print hotels data into table

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
 <?php 
 
+$hotels = [
 
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-
+    
+];
 
 ?>
 
@@ -11,9 +48,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- boostrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
     <title>PHP Hotel</title>
 </head>
-<body>
-    
+<body class="p-5">
+    <h1 class="fw-normal">PHP Hotel</h1>
+        <table class="table" > 
+            <thead>
+                <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrizione</th>
+                <th scope="col">Parcheggio</th>
+                <th scope="col">Voto</th>
+                <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($hotels as $hotel): ?>
+                <tr>
+                <td scope="row"><?php echo $hotel['name'] ?></th>
+                <td><?php echo $hotel['description'] ?></td>
+                <td><?php echo ($hotel['parking'] ? 'Si' : 'No') ?></td>
+                <td><?php echo $hotel['vote'] ?></td>
+                <td>km. <?php echo $hotel['distance_to_center'] ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+</div>
 </body>
 </html>
